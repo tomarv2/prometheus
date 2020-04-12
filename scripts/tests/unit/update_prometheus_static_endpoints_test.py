86d59a36d6cc7f08b624e6d208d6bc20f8933acd
tomarv2 <varuntomar2019@gmail.com>
2020-04-12 08:45:44 -0700

    initial commit

--- scripts/tests/unit/update_prometheus_static_endpoints_test.py
+++ scripts/tests/unit/update_prometheus_static_endpoints_test.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 import sys
-sys.path.insert(1, '/Users/vtomar/Documents/bitbucket/prometheus/scripts')
+sys.path.insert(1, 'prometheus/scripts')
 #import file_name_without_dot_py_at_end
 #from .update-prometheus-static-endpoints
 import update_values
